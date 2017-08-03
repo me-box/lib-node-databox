88fcd6bfb42ff64f6f9e3fac3260d52574d845c6
ToshBrown <Toshbrown@gmail.com>
2017-08-03 15:04:30 +0100

    Add store-timeseries API

--- databox.js
+++ databox.js
@@ -2,6 +2,7 @@ exports.waitForStoreStatus = require('./lib/utils.js').waitForStoreStatus;
 exports.getHttpsCredentials = require('./lib/utils.js').getHttpsCredentials;
 exports.catalog            = require('./lib/catalog.js');
 exports.timeseries         = require('./lib/time-series.js');
+exports.storetimeseries    = require('./lib/store-timeseries.js')
 exports.keyValue           = require('./lib/key-value.js');
 exports.subscriptions      = require('./lib/subscriptions.js');
 exports.export             = require('./lib/export.js');
