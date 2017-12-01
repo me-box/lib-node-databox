34dd5f02688fd7b4b882d0f534eb09700dbbdb98
ToshBrown <Toshbrown@gmail.com>
2017-12-01 11:10:20 +0000

    Update to nodezestclient 0.0.9 to fix utf8 payload length issue

--- test/core-store-ts-fast-writes.js
+++ test/core-store-ts-fast-writes.js
@@ -5,11 +5,12 @@ let serverEndPoint = "tcp://127.0.0.1:5555";
 
 describe('TS Client', function() {
 
-    let tsc = databox.NewTimeSeriesClient(serverEndPoint,false);
+    let tsc = databox.NewTimeSeriesClient(serverEndPoint,true);
     let someTimeInTheFuture = Date.now() + 10000;
     let dataSourceID = 'test' + Date.now(); //each test gets a fresh dataSourceID
 
-    let numRecordsToWrite = 100;
+
+    let numRecordsToWrite = 4;
 
     after(function () {
         tsc.zestClient.ZMQsoc.close();
@@ -19,16 +20,18 @@ describe('TS Client', function() {
       it('should write and resolve [created]', function() {
         let proms = [];
         for(let i = 0; i<numRecordsToWrite; i++) {
-            proms.push(tsc.Write(dataSourceID,{"test":"data"+i}));
+            proms.push(tsc.Write(dataSourceID,{"test":"data"+i}).then((resp)=>{console.log("Tosh:" + resp)}).catch((err)=>{console.log("ERROR writing: " + err);}));
         }
         return Promise.all(proms)
                 .then((res)=>{
+                    console.log('Done Writing at :: ',Date.now())
                     assert.equal(res.length,numRecordsToWrite);
                 });
       });
     });
 
     describe('#Latest', function() {
+        this.timeout(5000);
         it('should read latest value and return it', function() {
             console.log('Latest:: ',Date.now())
             return tsc.Latest(dataSourceID)
