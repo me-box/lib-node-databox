a21af37742ebae69e154068b3d0ce5badf08bf91
ToshBrown <Toshbrown@gmail.com>
2018-01-31 15:02:19 +0000

    Removes support for store-json

--- samples/app-hello-world/server.js
+++ samples/app-hello-world/server.js
@@ -8,31 +8,6 @@ const store = process.env.DATABOX_STORE_ENDPOINT;
 const credentials = databox.getHttpsCredentials()
 
 
-/*
-databox.catalog.listAvailableStores().then((stores) => console.log(stores));
-
-databox.catalog.walkStoreCatalogs().then((stores) => console.log(JSON.stringify(stores, null, '\t'))).catch((err) => console.error(err));
-
-databox.catalog.mapStoreCatalogs((cat, i) => {
-	console.log('Store ' + i + ':', JSON.stringify(cat, null, '\t'));
-}).catch((err) => console.error(err));
-
-databox.export.longpoll('https://export.amar.io/', { foo: 'bar' })
-	.then((response) => console.log(response))
-	.catch((err) => console.error(err));
-
-
-var notifications = databox.notifications.connect(href)
-
-notifications.on('open', () => {
-	//console.log('Store notificaitons connection open');
-});
-
-notificaitons.on('data', function (storeHostname, dataSourceID, data) {
-	//console.log(storeHostname, dataSourceID, data);
-});
-*/
-
 //start the https server for the App UI
 https.createServer(credentials, function (req, res) {
   //The https server is setup to offer the  UI for your App
