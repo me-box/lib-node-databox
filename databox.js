f2df6e8a73172c282397b6103a90f4d3959b769a
toshbrown <Toshbrown@gmail.com>
2018-01-31 15:03:07 +0000

    Removes support for store-json (#31)

--- databox.js
+++ databox.js
@@ -1,8 +1,3 @@
-exports.catalog            = require('./lib/catalog.js');
-exports.timeseries         = require('./lib/time-series.js');
-exports.keyValue           = require('./lib/key-value.js');
-exports.subscriptions      = require('./lib/subscriptions.js');
-
 //export service
 exports.export             = require('./lib/export.js');
 
